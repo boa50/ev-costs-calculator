6d595d5090a3a346767f8810d12bc96997e4bc2b
boa50 <bruno.albuquerque50@gmail.com>
2025-09-18 14:39:24 -0300

    Configured the app to use nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
 import { Text, View, TextInput } from 'react-native'
 
 export default function Index() {
-   return (
-      <View
-         style={{
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-         }}
-      >
-         <Text>Edit app/index.tsx to edit this screen.</Text>
-      </View>
-   )
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Text className="font-extrabold text-red-600 text-6xl">
+                It is already edited
+            </Text>
+        </View>
+    )
 }
