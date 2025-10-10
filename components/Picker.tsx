5e52ad1ada9819b1e7a421be72cc692c2a3cfdb2
boa50 <bruno.albuquerque50@gmail.com>
2025-10-10 13:45:30 -0300

    Created translation entries for the picker component

--- components/Picker.tsx
+++ components/Picker.tsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native'
 import RNPickerSelect from 'react-native-picker-select'
 import { InputLabel } from './InputLabel'
+import { useTranslation } from 'react-i18next'
 import colors from 'tailwindcss/colors'
 
 interface Props {
@@ -11,26 +12,26 @@ interface Props {
     hint?: string
 }
 
-const months = [
-    { label: 'January', value: '1' },
-    { label: 'February', value: '2' },
-    { label: 'March', value: '3' },
-    { label: 'April', value: '4' },
-    { label: 'May', value: '5' },
-    { label: 'June', value: '6' },
-    { label: 'July', value: '7' },
-    { label: 'August', value: '8' },
-    { label: 'September', value: '9' },
-    { label: 'October', value: '10' },
-    { label: 'November', value: '11' },
-    { label: 'December', value: '12' },
-]
-
 export function Picker({ label, value, setValue, type, hint }: Props) {
+    const { t } = useTranslation()
+
     let items = []
     switch (type) {
         case 'month':
-            items = months
+            items = [
+                { label: t('components.picker.january'), value: '1' },
+                { label: t('components.picker.february'), value: '2' },
+                { label: t('components.picker.march'), value: '3' },
+                { label: t('components.picker.april'), value: '4' },
+                { label: t('components.picker.may'), value: '5' },
+                { label: t('components.picker.june'), value: '6' },
+                { label: t('components.picker.july'), value: '7' },
+                { label: t('components.picker.august'), value: '8' },
+                { label: t('components.picker.september'), value: '9' },
+                { label: t('components.picker.october'), value: '10' },
+                { label: t('components.picker.november'), value: '11' },
+                { label: t('components.picker.december'), value: '12' },
+            ]
             break
     }
 
